0a5e5754102dc373646296909a0d49b228e5f348
Dylan Torres <diland.t95@hotmail.com>
2016-10-19 20:52:02 -0500

    Update files.py

--- ejercicios/ejercicioUno/files.py
+++ ejercicios/ejercicioUno/files.py
@@ -5,19 +5,16 @@ from files_commands import get_all_files, add_file, remove_file
 
 app = Flask(__name__)
 
+api_url = '/recently_created'
+
 @app.route('/files',methods=['POST'])
 def create_file():
   content = request.get_json(silent=True)
   filename = content['filename']
-  content = content['content']
-  if not filename or not content:
-    return "empty filename or content", 400
-  if filename in get_all_files():
-    return "file already exist", 400
-  if add_file(filename,content):
-    return "file created", 201
-  else:
-    return "error while creating file", 400
+  content =  content['content']
+  add_file(filename,content)  
+  return "Se creo",201
+  
 
 @app.route('/files',methods=['GET'])
 def read_user():
@@ -39,7 +36,7 @@ def delete_user():
   if error:
     return 'some files were not deleted', 400
   else:
-    return 'all files  were deleted', 200
+    return 'all files  were deleted', 200  	 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8088,debug='True')
