bfbca8f86a3e9b3a8d7f5488a52b833f2c6bcb7b
Dylan Torres <diland.t95@hotmail.com>
2016-10-19 20:59:03 -0500

    Update files_commands.py

--- ejercicios/ejercicioUno/files_commands.py
+++ ejercicios/ejercicioUno/files_commands.py
@@ -1,20 +1,21 @@
 from subprocess import Popen, PIPE
 
 def get_all_files():
-  grep_process = Popen(["ls","/home/filesystem_user"], stdout=PIPE, stderr=PIPE)
+  grep_process = Popen(["ls"], stdout=PIPE, stderr=PIPE)
   file_list = Popen(["awk","-F","/",'{print $1}'], stdin=grep_process.stdout, stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
   return filter(None,file_list)
 
 def add_file(filename,content):
-  add_process = Popen(["sudo","adduser","--password",password,filename], stdout=PIPE, stderr=PIPE)
-  add_process.wait()
-  return True if username in get_all_files() else False
+  add_process = Open(filename+'.txt','a')
+  add_process.write(content+'/n')
+  add_process.close()
+  return "Se creo el archivo" , 201
 
 def remove_file(filename):
-  vip = ["operativos","jenkins","postgres","root"]
+  vip = ["ambientes","los_repositorios","files","files_commands"]
   if filename in vip:
     return True
   else:
-    remove_process = Popen(["sudo","userdel","-r",filename], stdout=PIPE, stderr=PIPE)
+    remove_process = Popen(["rm","-r",filename], stdout=PIPE, stderr=PIPE)
     remove_process.wait()
     return False if filename in get_all_files() else True
